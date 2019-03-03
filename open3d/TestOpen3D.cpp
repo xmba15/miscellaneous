61930d47dc1c588f2da3d5a22869823c9922f6f2
xmba15 <thba1590@gmail.com>
2019-03-04 02:12:19 +0900

    add test file for open3d

---
+++ open3d/TestOpen3D.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file    TestOpen3D.cpp
+ *
+ * @brief   test open3d code
+ *
+ * @author  BT
+ *
+ * @date    2019-03-04
+ *
+ * framework
+ *
+ * Copyright (c) organization
+ *
+ */
+
+#include <Open3D/Open3D.h>
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
