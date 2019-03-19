33c8fb7f0fcc704e0d01a2e12de691ff90326df1
xmba15 <thba1590@gmail.com>
2019-03-19 18:42:49 +0900

    add conversion from Vec3b to python list

--- test_pybinding/TestPybinding.cpp
+++ test_pybinding/TestPybinding.cpp
@@ -13,13 +13,21 @@
 #include <opencv2/opencv.hpp>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <sstream>
+#include <stdexcept>
 #include <string>
+#include <typeinfo>
 
-struct inty { long long_value; };
+namespace py = pybind11;
 
-void print(inty s) {
-    std::cout << s.long_value << std::endl;
+struct inty {
+  long long_value;
+};
+
+void print(inty s)
+{
+  std::cout << s.long_value << std::endl;
 }
 
 int add(int i, int j)
@@ -34,7 +42,21 @@ void testRect(const cv::Rect &r)
   std::cout << ss.str() << "\n";
 }
 
-namespace py = pybind11;
+void testVec(const cv::Vec3b &v)
+{
+  uchar a = v[0];
+  std::cout << int(a) << "\n";
+}
+
+void testPoint(const cv::Point &p)
+{
+  std::cout << p.x << " " << p.y << "\n";
+}
+
+void testMat(const cv::Mat &img)
+{
+  std::cout << img.size << "\n";
+}
 
 PYBIND11_MODULE(example, m)
 {
@@ -59,68 +81,209 @@ PYBIND11_MODULE(example, m)
 
   m.def("testRect", &testRect, "A function to test cv::Rect");
   m.def("print", &print, "A function to test cv::Rect");
+  m.def("testVec", &testVec, "A function to test cv::Vec");
+  m.def("testPoint", &testPoint, "A function to test cv::Point");
+  m.def("testMat", &testMat, "A function to test cv::Mat");
 }
 
-// namespace pybind11
-// {
-// namespace detail
-// {
-// template <> struct type_caster<cv::Point> {
-//   PYBIND11_TYPE_CASTER(cv::Point, _("numpy.ndarray"));
+namespace pybind11
+{
+namespace detail
+{
+template <> struct type_caster<cv::Vec3b> {
+public:
+  PYBIND11_TYPE_CASTER(cv::Vec3b, _("cv::Vec3b"));
 
-//   // Cast numpy to cv::Point
-//   bool load(handle src, bool imp)
-//   {
-    
-//     return true;
-//   }
+  bool load(py::handle src, bool)
+  {
+    PyObject *source = src.ptr();
 
-//   // Cast cv::Point to numpy
-//   // static handle cast(const cv::Point &p, return_value_policy, handle defval)
-//   // {
-//   // }
-// }
-// }  // namespace detail
-// }  // namespace pybind11
+    py::list l = py::reinterpret_borrow<py::list>(source);
 
-namespace pybind11 { namespace detail {
-    template <> struct type_caster<inty> {
-    public:
-        /**
-         * This macro establishes the name 'inty' in
-         * function signatures and declares a local variable
-         * 'value' of type inty
-         */
-        PYBIND11_TYPE_CASTER(inty, _("inty"));
+    value[0] = l[0].cast<uchar>();
+    value[1] = l[1].cast<uchar>();
+    value[2] = l[2].cast<uchar>();
 
-        /**
-         * Conversion part 1 (Python->C++): convert a PyObject into a inty
-         * instance or return false upon failure. The second argument
-         * indicates whether implicit conversions should be applied.
-         */
-        bool load(handle src, bool) {
-            /* Extract PyObject from handle */
-            PyObject *source = src.ptr();
-            /* Try converting into a Python integer value */
-            PyObject *tmp = PyNumber_Long(source);
-            if (!tmp)
-                return false;
-            /* Now try to convert into a C++ int */
-            value.long_value = PyLong_AsLong(tmp);
-            Py_DECREF(tmp);
-            /* Ensure return code was OK (to avoid out-of-range errors etc) */
-            return !(value.long_value == -1 && !PyErr_Occurred());
-        }
+    return true;
+  }
 
-        /**
-         * Conversion part 2 (C++ -> Python): convert an inty instance into
-         * a Python object. The second and third arguments are used to
-         * indicate the return value policy and parent object (for
-         * ``return_value_policy::reference_internal``) and are generally
-         * ignored by implicit casters.
-         */
-        static handle cast(inty src, return_value_policy /* policy */, handle /* parent */) {
-            return PyLong_FromLong(src.long_value);
-        }
-    };
-}} // namespace pybind11::detail
+  static py::handle cast(const cv::Vec3b &vec, py::return_value_policy,
+                         py::handle)
+  {
+    py::list l;
+    l.append(vec[0]);
+    l.append(vec[1]);
+    l.append(vec[2]);
+    return l.release();
+  }
+};
+
+template <class T> void insertValue(cv::Point &p, py::buffer_info &info)
+{
+  std::vector<T> v(static_cast<T *>(info.ptr),
+                   static_cast<T *>(info.ptr) + info.shape[0]);
+  p.x = v[0];
+  p.y = v[1];
+}
+
+template <> struct type_caster<cv::Point> {
+public:
+  PYBIND11_TYPE_CASTER(cv::Point, _("numpy.ndarray"));
+
+  bool load(py::handle src, bool)
+  {
+    if (!src) {
+      return false;
+    }
+
+    if (!py::isinstance<py::array>(src)) {
+      throw std::runtime_error(
+          "Incompatible type of Point intput. Use numpy array instead");
+    }
+
+    py::array parray = reinterpret_borrow<py::array>(src);
+    py::buffer_info info = parray.request();
+
+    if (info.ndim != 1) {
+      throw std::runtime_error("Number of dimension must be one");
+    }
+
+    if (info.shape[0] != 2) {
+      throw std::runtime_error("Input shapes must be 2");
+    }
+
+    if (info.format == py::format_descriptor<double>::format()) {
+      insertValue<double>(value, info);
+    } else if (info.format == py::format_descriptor<float>::format()) {
+      insertValue<float>(value, info);
+    } else if (info.format == py::format_descriptor<int16_t>::format()) {
+      insertValue<int16_t>(value, info);
+    } else if (info.format == py::format_descriptor<int32_t>::format()) {
+      insertValue<int32_t>(value, info);
+    } else {
+      insertValue<int64_t>(value, info);
+    }
+
+    return true;
+  }
+
+  static py::handle cast(const cv::Point &p, py::return_value_policy,
+                         py::handle)
+  {
+    std::vector<int> v(p.x, p.y);
+    py::array_t<int> parray = py::array_t<int>(v.size());
+    py::buffer_info info = parray.request();
+    int *infoPtr = static_cast<int *>(info.ptr);
+    std::memcpy(infoPtr, v.data(), v.size() * sizeof(int));
+
+    return parray.release();
+  }
+};
+
+template <> struct type_caster<cv::Mat> {
+public:
+  PYBIND11_TYPE_CASTER(cv::Mat, _("numpy.ndarray"));
+
+  bool load(handle src, bool)
+  {
+    if (!src) {
+      return false;
+    }
+
+    if (!py::isinstance<py::array>(src)) {
+      throw std::runtime_error(
+          "Incompatible type of Mat intput. Use numpy array instead");
+    }
+
+    py::array b = py::reinterpret_borrow<py::array>(src);
+    buffer_info info = b.request();
+
+    int ndims = info.ndim;
+
+    decltype(CV_32F) dtype;
+    size_t elemsize;
+    if (info.format == py::format_descriptor<float>::format()) {
+      if (ndims == 3) {
+        dtype = CV_32FC3;
+      } else {
+        dtype = CV_32FC1;
+      }
+      elemsize = sizeof(float);
+    } else if (info.format == py::format_descriptor<double>::format()) {
+      if (ndims == 3) {
+        dtype = CV_64FC3;
+      } else {
+        dtype = CV_64FC1;
+      }
+      elemsize = sizeof(double);
+    } else if (info.format == py::format_descriptor<unsigned char>::format()) {
+      if (ndims == 3) {
+        dtype = CV_8UC3;
+      } else {
+        dtype = CV_8UC1;
+      }
+      elemsize = sizeof(unsigned char);
+    } else {
+      throw std::runtime_error("Unsupported type");
+    }
+
+    std::vector<int> shape = {static_cast<int>(info.shape[0]),
+                              static_cast<int>(info.shape[1])};
+
+    value = cv::Mat(cv::Size(shape[1], shape[0]), dtype, info.ptr,
+                    cv::Mat::AUTO_STEP);
+    return true;
+  }
+
+  static handle cast(const cv::Mat &m, return_value_policy, handle defval)
+  {
+    std::string format = py::format_descriptor<unsigned char>::format();
+    size_t elemsize = sizeof(unsigned char);
+    int dim;
+    switch (m.type()) {
+      case CV_8U:
+        format = py::format_descriptor<unsigned char>::format();
+        elemsize = sizeof(unsigned char);
+        dim = 2;
+        break;
+      case CV_8UC3:
+        format = py::format_descriptor<unsigned char>::format();
+        elemsize = sizeof(unsigned char);
+        dim = 3;
+        break;
+      case CV_32F:
+        format = py::format_descriptor<float>::format();
+        elemsize = sizeof(float);
+        dim = 2;
+        break;
+      case CV_64F:
+        format = py::format_descriptor<double>::format();
+        elemsize = sizeof(double);
+        dim = 2;
+        break;
+      default:
+        throw std::runtime_error("Unsupported type");
+    }
+
+    std::vector<size_t> bufferdim;
+    std::vector<size_t> strides;
+    if (dim == 2) {
+      bufferdim = {(size_t)m.rows, (size_t)m.cols};
+      strides = {elemsize * (size_t)m.cols, elemsize};
+    } else if (dim == 3) {
+      bufferdim = {(size_t)m.rows, (size_t)m.cols, (size_t)3};
+      strides = {(size_t)elemsize * m.cols * 3, (size_t)elemsize * 3,
+                 (size_t)elemsize};
+    }
+    return array(buffer_info(m.data,   /* Pointer to buffer */
+                             elemsize, /* Size of one scalar */
+                             format, /* Python struct-style format descriptor */
+                             dim,    /* Number of dimensions */
+                             bufferdim, /* Buffer dimensions */
+                             strides    /* Strides (in bytes) for each index */
+                             ))
+        .release();
+  }
+};
+}  // namespace detail
+}  // namespace pybind11
