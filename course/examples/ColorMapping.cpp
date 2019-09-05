fffc34bc8d86a031033c3b00e3720be22a359d0d
xmba15 <thba1590@gmail.com>
2019-09-05 23:40:41 +0900

    foundation for multimedia

---
+++ course/examples/ColorMapping.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file    ColorMapping.cpp
+ *
+ * @author  btran
+ *
+ * @date    2019-09-06
+ *
+ * Copyright (c) organization
+ *
+ */
+
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
