5eadf23185cd0f2727fbfc54d78e7d2d8c1cc21c
xmba15 <thba1590@gmail.com>
2019-09-05 19:26:00 +0900

    add color mapping

---
+++ courses/docs/README.md
@@ -0,0 +1,5 @@
+# Useful URLs #
+- [path planning by Maxim Likhachev](http://www.cs.cmu.edu/~maxim/)
+- [motion planning by Howie Choset](http://www.cs.cmu.edu/~./motionplanning/)
+- [list of course uisng the textbook "Principles of Robot Motion"](http://biorobotics.ri.cmu.edu/book/)
+- [list of books on decision making and planning](https://github.com/yangmingustb/planning_books_1)
---
+++ courses/examples/ColorMapping.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file    ColorMapping.cpp
+ *
+ * @author  btran
+ *
+ * @date    2019-09-05
+ *
+ * Copyright (c) organization
+ *
+ */
+
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[])
+{
+#ifdef DATA_PATH
+    const std::string IMAGE_PATH = std::string(DATA_PATH) + "/teddy/";
+    const std::string FIRST = IMAGE_PATH + "im2.png";
+    const std::string SECOND = IMAGE_PATH + "im6.png";
+
+    const cv::Mat first = cv::imread(FIRST);
+    const cv::Mat second = cv::imread(second);
+
+
+
+#endif  // DATA_PATH
+
+    return 0;
+}
